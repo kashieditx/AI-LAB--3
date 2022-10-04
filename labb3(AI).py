1bd7b0803178b0ab1c20465c3702452cf5b19a90
kashieditx <113879405+kashieditx@users.noreply.github.com>
2022-10-04 11:06:04 +0500

    Add files via upload

---
+++ labb3(AI).py
@@ -0,0 +1,368 @@
+                #Activity1
+
+"""
+class Node:
+    #class variable shared by all
+    def _init_(self, state, parent,actions,totalcost):
+        self.state = state
+        self.parent= parent
+        self.actions = actions
+        #not save actions but output state of actions
+        self.totalcost = totalcost
+#graph = dictionary
+#nodes with key and value
+#key is state
+#value actual attribute
+graph={'A' : Node {'A',None ,['B', 'C' , 'E'],None},
+       'B' : Node {'B',None ,['A', 'D' , 'E'],None},
+       'C' : Node {'C',None ,['A', 'F' , 'G'],None},
+       'D' : Node {'D',None ,['B', 'E' ],None},
+       'E' : Node {'E',None ,['A', 'B' , 'E'],None},
+       'F' : Node {'F',None ,['C'],None},
+       'G' : Node {'G',None ,['C'],None}} """
+"""
+graph={'A' : ['B', 'C' , 'E'],
+       'B' : ['A', 'D' , 'E'],
+       'C' :['A', 'F' , 'G'],
+       'D' :['B', 'E' ],
+       'E' :['A', 'B' , 'E'],
+       'F' :['C'],
+       'G' :['C']}
+print(graph())"""
+
+
+
+                 #Activity2
+class Node:
+    #class variable shared by all
+    def _init_(self, state, parent,actions,totalcost):
+        self.state = state
+        self.parent= parent
+        self.actions = actions
+        #not save actions but output state of actions
+        self.totalcost = totalcost
+
+
+def BFS():
+    initialState = 'D'
+    goalState = 'F'
+    graph={'A' : Node{'A',None ,['B', 'C' , 'E'],None},
+       'B' : Node {'B',None ,['A', 'D' , 'E'],None},
+       'C' : Node {'C',None ,['A', 'F' , 'G'],None},
+       'D' : Node {'D',None ,['B', 'E' ],None},
+       'E' : Node {'E',None ,['A', 'B' , 'E'],None},
+       'F' : Node {'F',None ,['C'],None},
+       'G' : Node {'G',None ,['C'],None}}
+    frontier = [initialState]
+    explored =[]
+while len(frontier) !=0:
+    currentNode = frontier.pop(0)
+    explored.append(currentNode)
+    for child in graph[currentNode].actions:
+        if child not in frontier and child not in explored:
+            graph[child].parent = currentNode
+            if graph[child].state == goalState:
+                return actionSequence(graph,initialState,goalState)
+            frontier.append(child)
+solution = BFSs()
+print(solution)
+def actionSequence (graph, initialState, goalState):
+    solution = [goalState]
+    currentParent = graph[goalState].parent
+    while currentParent!=None:
+        solution.append(currentParent)
+        currentParent = graph[currentParent].parent
+    solution.reverse()
+    return solution
+
+
+
+
+
+
+
+                           #LabTask1
+
+from queue import PriorityQueue
+v = 14
+graph = [[] for i in range(v)]
+
+# Function For Implementing Best First Search
+# Gives output path having lowest cost
+
+
+def best_first_search(actual_Src, target, n):
+	visited = [False] * n
+	pq = PriorityQueue()
+	pq.put((0, actual_Src))
+	visited[actual_Src] = True
+	
+	while pq.empty() == False:
+		u = pq.get()[1]
+		# Displaying the path having lowest cost
+		print(u, end=" ")
+		if u == target:
+			break
+
+		for v, c in graph[u]:
+			if visited[v] == False:
+				visited[v] = True
+				pq.put((c, v))
+	print()
+
+# Function for adding edges to graph
+
+
+def addedge(x, y, cost):
+	graph[x].append((y, cost))
+	graph[y].append((x, cost))
+
+
+# The nodes shown in above example(by alphabets) are
+# implemented using integers addedge(x,y,cost);
+addedge(0, 1, 3)
+addedge(0, 2, 6)
+addedge(0, 3, 5)
+addedge(1, 4, 9)
+addedge(1, 5, 8)
+addedge(2, 6, 12)
+addedge(2, 7, 14)
+addedge(3, 8, 7)
+addedge(8, 9, 5)
+addedge(8, 10, 6)
+addedge(9, 11, 1)
+addedge(9, 12, 10)
+addedge(9, 13, 2)
+
+source = 0
+target = 9
+best_first_search(source, target, v)
+
+# This code is contributed by Jyotheeswar Ganne
+
+
+
+
+
+                           #Task2
+
+
+# Python3 program to print the path from root
+# node to destination node for N*N-1 puzzle
+# algorithm using Branch and Bound
+# The solution assumes that instance of
+# puzzle is solvable
+
+# Importing copy for deepcopy function
+import copy
+
+# Importing the heap functions from python
+# library for Priority Queue
+from heapq import heappush, heappop
+
+# This variable can be changed to change
+# the program from 8 puzzle(n=3) to 15
+# puzzle(n=4) to 24 puzzle(n=5)...
+n = 3
+
+# bottom, left, top, right
+row = [ 1, 0, -1, 0 ]
+col = [ 0, -1, 0, 1 ]
+
+# A class for Priority Queue
+class priorityQueue:
+	
+	# Constructor to initialize a
+	# Priority Queue
+	def __init__(self):
+		self.heap = []
+
+	# Inserts a new key 'k'
+	def push(self, k):
+		heappush(self.heap, k)
+
+	# Method to remove minimum element
+	# from Priority Queue
+	def pop(self):
+		return heappop(self.heap)
+
+	# Method to know if the Queue is empty
+	def empty(self):
+		if not self.heap:
+			return True
+		else:
+			return False
+
+# Node structure
+class node:
+	
+	def __init__(self, parent, mat, empty_tile_pos,
+				cost, level):
+					
+		# Stores the parent node of the
+		# current node helps in tracing
+		# path when the answer is found
+		self.parent = parent
+
+		# Stores the matrix
+		self.mat = mat
+
+		# Stores the position at which the
+		# empty space tile exists in the matrix
+		self.empty_tile_pos = empty_tile_pos
+
+		# Stores the number of misplaced tiles
+		self.cost = cost
+
+		# Stores the number of moves so far
+		self.level = level
+
+	# This method is defined so that the
+	# priority queue is formed based on
+	# the cost variable of the objects
+	def __lt__(self, nxt):
+		return self.cost < nxt.cost
+
+# Function to calculate the number of
+# misplaced tiles ie. number of non-blank
+# tiles not in their goal position
+def calculateCost(mat, final) -> int:
+	
+	count = 0
+	for i in range(n):
+		for j in range(n):
+			if ((mat[i][j]) and
+				(mat[i][j] != final[i][j])):
+				count += 1
+				
+	return count
+
+def newNode(mat, empty_tile_pos, new_empty_tile_pos,
+			level, parent, final) -> node:
+				
+	# Copy data from parent matrix to current matrix
+	new_mat = copy.deepcopy(mat)
+
+	# Move tile by 1 position
+	x1 = empty_tile_pos[0]
+	y1 = empty_tile_pos[1]
+	x2 = new_empty_tile_pos[0]
+	y2 = new_empty_tile_pos[1]
+	new_mat[x1][y1], new_mat[x2][y2] = new_mat[x2][y2], new_mat[x1][y1]
+
+	# Set number of misplaced tiles
+	cost = calculateCost(new_mat, final)
+
+	new_node = node(parent, new_mat, new_empty_tile_pos,
+					cost, level)
+	return new_node
+
+# Function to print the N x N matrix
+def printMatrix(mat):
+	
+	for i in range(n):
+		for j in range(n):
+			print("%d " % (mat[i][j]), end = " ")
+			
+		print()
+
+# Function to check if (x, y) is a valid
+# matrix coordinate
+def isSafe(x, y):
+	
+	return x >= 0 and x < n and y >= 0 and y < n
+
+# Print path from root node to destination node
+def printPath(root):
+	
+	if root == None:
+		return
+	
+	printPath(root.parent)
+	printMatrix(root.mat)
+	print()
+
+# Function to solve N*N - 1 puzzle algorithm
+# using Branch and Bound. empty_tile_pos is
+# the blank tile position in the initial state.
+def solve(initial, empty_tile_pos, final):
+	
+	# Create a priority queue to store live
+	# nodes of search tree
+	pq = priorityQueue()
+
+	# Create the root node
+	cost = calculateCost(initial, final)
+	root = node(None, initial,
+				empty_tile_pos, cost, 0)
+
+	# Add root to list of live nodes
+	pq.push(root)
+
+	# Finds a live node with least cost,
+	# add its children to list of live
+	# nodes and finally deletes it from
+	# the list.
+	while not pq.empty():
+
+		# Find a live node with least estimated
+		# cost and delete it form the list of
+		# live nodes
+		minimum = pq.pop()
+
+		# If minimum is the answer node
+		if minimum.cost == 0:
+			
+			# Print the path from root to
+			# destination;
+			printPath(minimum)
+			return
+
+		# Generate all possible children
+		for i in range(4):
+			new_tile_pos = [
+				minimum.empty_tile_pos[0] + row[i],
+				minimum.empty_tile_pos[1] + col[i], ]
+				
+			if isSafe(new_tile_pos[0], new_tile_pos[1]):
+				
+				# Create a child node
+				child = newNode(minimum.mat,
+								minimum.empty_tile_pos,
+								new_tile_pos,
+								minimum.level + 1,
+								minimum, final,)
+
+				# Add child to list of live nodes
+				pq.push(child)
+
+# Driver Code
+
+# Initial configuration
+# Value 0 is used for empty space
+initial = [ [ 1, 2, 3 ],
+			[ 5, 6, 0 ],
+			[ 7, 8, 4 ] ]
+
+# Solvable Final configuration
+# Value 0 is used for empty space
+final = [ [ 1, 2, 3 ],
+		[ 5, 8, 6 ],
+		[ 0, 7, 4 ] ]
+
+# Blank tile coordinates in
+# initial configuration
+empty_tile_pos = [ 1, 2 ]
+
+# Function call to solve the puzzle
+solve(initial, empty_tile_pos, final)
+
+# This code is contributed by Kevin Joshi
+
+
+
+
+
+
+
+
